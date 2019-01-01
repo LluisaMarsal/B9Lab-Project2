21dc77e299eea1b02a50649289b65549706daab9
LluisaMarsal <llmarsal@intelligenter.org>
2019-01-01 19:29:48 +0100

    Remittance: rearranging deadline and removing owner from structs

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -6,20 +6,20 @@ contract Remittance is Pausable {
     
     uint constant fee = 50;
     uint constant maxDurationInBlocks = 4 weeks / 15;
+    uint constant minDurationInBlocks = 1 days / 15;
     
     struct RemittanceBox {
        address sentFrom;
        address moneyChanger; 
-       address owner;
        uint amount;
        uint deadline; 
     }
     // for every bytes32 there is a RemittanceBox and those namespaces (struct) will conform a mapping named remittanceStructs
     mapping (bytes32 => RemittanceBox) public remittanceStructs; 
 
-    event LogDeposit(address indexed sentFrom, address indexed moneyChanger, uint amount, uint fee, uint numberOfBlocks);
-    event LogCollect(address indexed moneyChanger, uint amount);
-    event LogCancel(address indexed sentFrom, uint amount);
+    event LogDeposit(address indexed sentFrom, address indexed moneyChanger, uint amount, uint fee, uint numberOfBlocks, uint blockNumber);
+    event LogCollect(address indexed moneyChanger, uint amount, uint blockNumber);
+    event LogCancel(address indexed sentFrom, uint amount, uint blockNumber);
     
     function Remittance() public {
     }
@@ -29,18 +29,19 @@ contract Remittance is Pausable {
     }
     
     function depositRemittance(bytes32 hashedPassword, address moneyChanger, address owner, uint numberOfBlocks) public payable onlyIfRunning returns(bool success) {
+        require(hashedPassword != 0);
         require(remittanceStructs[hashedPassword].amount == 0);
         require(remittanceStructs[hashedPassword].moneyChanger != 0x0);
+        require(owner != 0x0);
         require(remittanceStructs[hashedPassword].deadline > 0);
         require(msg.value > fee);
         require(numberOfBlocks > 1 days / 15);
         require(numberOfBlocks < 4 weeks / 15);
-        remittanceStructs[hashedPassword].owner = owner;
         remittanceStructs[hashedPassword].moneyChanger = moneyChanger;
         remittanceStructs[hashedPassword].sentFrom = msg.sender;
         remittanceStructs[hashedPassword].amount = msg.value - fee;
         remittanceStructs[hashedPassword].deadline = block.number + numberOfBlocks;
-        LogDeposit(msg.sender, moneyChanger, msg.value, fee, numberOfBlocks);
+        LogDeposit(msg.sender, moneyChanger, msg.value, fee, numberOfBlocks, block.number);
         owner.transfer(fee);
         return true;
     }
@@ -50,9 +51,9 @@ contract Remittance is Pausable {
         uint amount = remittanceStructs[hashedPassword].amount;
         require(amount != 0);
         require(remittanceStructs[hashedPassword].moneyChanger == msg.sender);
-        require(remittanceStructs[hashedPassword].deadline >= block.number + maxDurationInBlocks);
+        require(remittanceStructs[hashedPassword].deadline >= block.number);
         remittanceStructs[hashedPassword].amount = 0;
-        LogCollect(msg.sender, remittanceStructs[hashedPassword].amount);
+        LogCollect(msg.sender, remittanceStructs[hashedPassword].amount, block.number);
         msg.sender.transfer(amount);
         return true;
     }
@@ -60,10 +61,10 @@ contract Remittance is Pausable {
     function cancelRemittance(bytes32 hashedPassword) public onlyIfRunning returns(bool success) {
         require(remittanceStructs[hashedPassword].amount != 0);
         require(remittanceStructs[hashedPassword].sentFrom == msg.sender);
-        require(remittanceStructs[hashedPassword].deadline < block.number + maxDurationInBlocks); 
+        require(remittanceStructs[hashedPassword].deadline < block.number); 
         uint amount = remittanceStructs[hashedPassword].amount;
         remittanceStructs[hashedPassword].amount = 0;
-        LogCancel(msg.sender, amount);
+        LogCancel(msg.sender, amount, block.number);
         msg.sender.transfer(amount);
         return true;
     }
